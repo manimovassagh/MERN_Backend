a6fe9539729fda836929170ee1089ffd89266e7d
manimovassagh <mani.mgh@gmail.com>
2021-10-15 18:09:07 +0200

    add some context

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,47 @@
-import React,{useReaducer } from 'react';
+import React, { useReaducer } from 'react';
 import uuid from 'uuid';
 import contactContext from './ContactContext';
 import contactReducer from './contactReducer';
+import {
+    GET_CONTACTS,
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_CONTACTS,
+    CLEAR_FILTER,
+    CONTACT_ERROR
+} from '../types'
 
+
+const contactState=props=>{
+    const initialState={
+        contacts:[
+            {
+            id:1,
+            name:"Mani",
+            email:"mani.mgh@gmail.com",
+            phone:"0173445",
+            type:"professional"
+        },
+            {
+            id:2,
+            name:"Sahar",
+            email:"sahar.mgh@gmail.com",
+            phone:"6786353",
+            type:"personal"
+        },
+            {
+            id:3,
+            name:"Mehdi",
+            email:"mehdi.rahmaniyan@gmail.com",
+            phone:"334789456",
+            type:"professional"
+        }
+    
+    
+    ]
+    }
+}
