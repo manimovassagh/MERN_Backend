cc8e819a70775e499b4d4053b7b6075f818529b3
manimovassagh <mani.mgh@gmail.com>
2021-10-18 17:32:33 +0200

    login Links at top and pages added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 //add some branch
 const App = () => {
@@ -13,12 +15,14 @@ const App = () => {
     <AuthState>
     <ContactState>
     <Router>
-      <Fragment>
+          <Fragment>
         <Navbar />
         <div className="container">
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </Fragment>
